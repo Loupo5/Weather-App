68dc109fefd03989a891d5025242a54d80f4bc9b
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-08-13 20:55:44 +0200

    Make fetching data based on inputed location

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+const form = document.querySelector("form")
+const locationInput = document.getElementById("loc")
+const search = document.getElementById("search-btn")
 
-async function fetchData() {
-    const fetched = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London/?key=93AYJZ55PKFC6XT7D5FMLZGF6&unitGroup=uk`)
-    const result = await fetched.json()
-    console.log(`${result.currentConditions.feelslike} Celsius`)
+
+async function fetchData(location) {
+    try {
+        const fetched = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location.value}/?key=93AYJZ55PKFC6XT7D5FMLZGF6&unitGroup=uk`)
+        const result = await fetched.json()
+        console.log(`${result.currentConditions.feelslike} Celsius`)
+    } catch(error) {
+        console.error("Couldnt fetch location", error)
+    }
 }
-fetchData()
+
+form.addEventListener("submit", (e) => {
+    fetchData(locationInput)
+})
